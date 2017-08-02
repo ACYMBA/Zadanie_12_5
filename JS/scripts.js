5cabad8c915170519f01369805497c5faebd96bc
albert <albert.cymbaluk@gmail.com>
2017-08-02 21:23:25 +0200

    scripts

--- JS/scripts.js
+++ JS/scripts.js
@@ -10,10 +10,23 @@ function createTweet(input) {
 
     var quoteText = $(data.content).text().trim();
     var quoteAuthor = data.title;
-    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;Copy
 
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
-  
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+	if (tweetText.length > 140) {
+	    getQuote();
+	} else {
+	    var tweet = tweetLink + encodeURIComponent(tweetText);
+	    $('.quote').text(quoteText);
+	    $('.author').text("Author: " + quoteAuthor);
+	    $('.tweet').attr('href', tweet);
+	}
+		$(document).ready(function() {
+		    getQuote();
+		    $('.trigger').click(function() {
+		        getQuote();
+		    })
+		});
 }
