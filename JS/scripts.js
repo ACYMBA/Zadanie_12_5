9c3c7a4e8e7279f8a332866be235c86b1c5d4df4
albert <albert.cymbaluk@gmail.com>
2017-08-07 12:17:43 +0200

    correction

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,19 +1,20 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var prefix = "https://cors-anywhere.herokuapp.com/";
 
 function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
+   $.getJSON(prefix + quoteUrl, createTweet);
 }
 
 function createTweet(input) {
     var data = input[0];
-
     var quoteText = $(data.content).text().trim();
     var quoteAuthor = data.title;
 
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
+
     var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 	if (tweetText.length > 140) {
 	    getQuote();
@@ -23,10 +24,12 @@ function createTweet(input) {
 	    $('.author').text("Author: " + quoteAuthor);
 	    $('.tweet').attr('href', tweet);
 	}
-		$(document).ready(function() {
-		    getQuote();
-		    $('.trigger').click(function() {
-		        getQuote();
-		    })
-		});
+
 }
+
+	$(document).ready(function() {
+	    getQuote();
+	    $('.trigger').click(function() {
+	        getQuote();
+	})
+});
